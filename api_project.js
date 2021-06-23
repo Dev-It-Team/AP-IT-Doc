1e6191e8d02ce18fcb9896495a16ad6a8f329323
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2021-06-23 09:11:04 +0200

    Add first version of the API

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "AP'IT",
+  "version": "0.0.0",
+  "description": "Documentation of AP'IT, developed by Dev'IT",
+  "title": "AP'IT Doc",
+  "url": "https://github.com/Dev-It-Team/AP-IT/",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-06-23T07:01:36.903Z",
+    "url": "https://apidocjs.com",
+    "version": "0.28.1"
+  }
+});
